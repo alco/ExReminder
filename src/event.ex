9d7f6180808da9438ebefd6b1d7e92e945b35a13
Alexei Sholik <alcosholik@gmail.com>
2012-03-29 18:59:39 +0300

    Refactor event.ex and add a test example

--- src/event.ex
+++ src/event.ex
@@ -1,18 +1,22 @@
+#
+# Every event in our reminder app is a separate Event process.
+# It basically sits inside the 'receive' block waiting for :cancel message.
+# When the timeout has passed and no :cancel message has been received,
+# it sends the :done message to the server.
+#
+# While this module depends on a server, it does not known anything about it.
+# Therefore, we can easily test it from the shell or by implementing a minimal
+# server to exercise its API.
+#
 defmodule Event do
     defrecord Event.State, server: nil, name: "", to_go: 0
 
+    # This sets up an alias for the record so that we can simply use State to
+    # refer to it inside the module
     refer Event.State
 
-    def msg_loop(state) do
-        server = state.server
-        timeout = state.to_go * 1000
-        receive do
-        match: {^server, ref, :cancel}
-            server <- { ref, :ok }
-        after: ^timeout
-            server <- { :done, state.name }
-        end
-    end
+
+    ## Public API ##
 
     def start(event_name, delay) do
         :erlang.spawn __MODULE__, :init, [Process.self, event_name, delay]
@@ -23,19 +27,39 @@ defmodule Event do
     end
 
     def init(server, event_name, delay) do
-        msg_loop State.new server: server, name: event_name, to_go: delay
+        main_loop State.new server: server, name: event_name, to_go: delay
     end
 
     def cancel(pid) do
-        # Monitor in case the process is already dead
-        ref = :erlang.monitor :process, pid
-        pid <- { Process.self, ref, :cancel }
+        # Create a monitor to know when the process dies
+        mon = :erlang.monitor :process, pid
+        pid <- { Process.self, mon, :cancel }
         receive do
-        match: { ^ref, :ok }
-            :erlang.demonitor ref, [:flush]
+        match: { ^mon, :ok }
+            :erlang.demonitor mon, [:flush]
             :ok
-        match: { DOWN, ^ref, :process, ^pid, _reason }
+        match: { DOWN, ^mon, :process, ^pid, _reason }
+            # The server is down. We're ok with that.
             :ok
+        end
+    end
+
+
+    ## Private functions ##
+
+    # This is not actually a loop, but it is all the same the main
+    # function in which our process spends most of the time
+    defp main_loop(state) do
+        server = state.server
+        receive do
+        match: {^server, ref, :cancel}
+            # After sending :ok to the server, we leave this function basically
+            # terminating the process. Thus, no reminder shall be sent.
+            server <- { ref, :ok }
+
+        after: state.to_go * 1000
+            # The timeout has passed, now is the time to remind the server.
+            server <- { :done, state.name }
         end
     end
 end
---
+++ test_event.exs
@@ -0,0 +1,34 @@
+#
+# Exercise the public API of the Event module
+#
+# First, compile the event.ex file. Then run
+#
+#     $ iex test_event.exs
+#
+
+
+# Flushes all currently available messages to standard output.
+flush = fn() ->
+    f = fn(f) ->
+        receive do
+        match: x
+            s = inspect x
+            IO.puts "Shell got #{s}"
+            f.(f)
+        after: 0
+            :ok
+        end
+    end
+    f.(f)
+end
+
+# Spawn a new Event process
+Event.start "Event", 0
+
+# Print out all pending messages
+flush.()
+
+# This time we keep a reference to the new process around so that we can
+# cancel it later
+pid = Event.start "Another event", 500
+Event.cancel pid
