d12ec179dd5e77aa94c2dd37810a06dc03f7994a
Alexei Sholik <alcosholik@gmail.com>
2012-03-29 19:28:00 +0300

    Refactor event_server.ex

--- src/event_server.ex
+++ src/event_server.ex
@@ -1,87 +1,23 @@
+#
+# This is our singleton event server. It implements messaging with clients as
+# well as Event processes. Details of the messaging protocol are hidden behind
+# a simple API.
+#
 defmodule EventServer do
-    # We need to keep a list of all subscribe clients
-    defrecord EventServer.State, events: nil, clients: nil
-
-    # We also need to keep track of all event processes we spawn
-    defrecord EventServer.Event, name: "", description: "", pid: nil, timeout: 0
-
+    # We need to keep a list of all pending events and subscribed clients
+    defrecord EventServer.State, events: [], clients: []
     refer EventServer.State
+
+    # Event description
+    defrecord EventServer.Event, name: "", description: "", pid: nil, timeout: 0
     refer EventServer.Event
 
-    # Main receive loop
-    def msg_loop(state) do
-        receive do
-        match: { pid, msg_ref, {:subscribe, client} }
-            # We'll be keeping a list of all subscribers and monitor them
-            # so that we don't send useless messages to crashed clients
-            ref = :erlang.monitor :process, client
-            new_state = state.update_clients fn(clients) -> :orddict.store ref, client, clients end
-            pid <- { msg_ref, :ok }
-            msg_loop new_state
 
-        match: { pid, msg_ref, {:add, name, description, timeout} }
-            # TODO: validate the timeout value
-            if timeout > 0 do
-                event_pid = __MAIN__.Event.start_link name, timeout
-                new_state = state.update_events fn(events) ->
-                                :orddict.store name,
-                                Event.new(name: name, description: description, pid: event_pid, timeout: timeout),
-                                events
-                            end
-                pid <- { msg_ref, :ok }
-                msg_loop new_state
-            else:
-                pid <- { msg_ref, {:error, :bad_timeout} }
-                msg_loop state
-            end
-
-        match: { pid, msg_ref, {:cancel, name} }
-            # If the event with the specified name is not found
-            # we simply do nothing
-            events = case :orddict.find name, state.events do
-                        match: { :ok, event }
-                            __MAIN__.Event.cancel event.pid
-                            :orddict.erase name, state.events
-                        match: :error
-                            state.events
-                     end
-            pid <- { msg_ref, :ok }
-            msg_loop state.events events
-
-        match: { :done, name }
-            #
-            case :orddict.find name, state.events do
-            match: { :ok, event }
-                send_to_clients { :done, event.name, event.description }, state.clients
-                msg_loop state.update_events fn(events) -> :orddict.erase name, events end
-            match: :error
-                # we cancel an event and it fires at the same time
-                msg_loop state
-            end
-
-        match: :shutdown
-            #
-            exit :shutdown
-
-        match: { 'DOWN', ref, :process, _pid, _reason }
-            #
-            msg_loop state.update_clients fn(clients) -> :orddict.erase ref, clients end
-
-        match: :code_change
-            #
-            __MODULE__.msg_loop state
-
-        match: else
-            :io.format "Unknown message: ~p~n", [else]
-            msg_loop state
-        end
-    end
-
-    def init do
-        msg_loop State.new events: :orddict.new, clients: :orddict.new
-    end
+    ## Public API ##
 
     def start do
+        # Register the server process so that it can be referred to as
+        # EventServer by clients
         :erlang.register __MODULE__, pid = :erlang.spawn __MODULE__, :init, []
         pid
     end
@@ -91,56 +27,143 @@ defmodule EventServer do
         pid
     end
 
-    def terminate do
-        __MODULE__ <- :shutdown
+    def init do
+        main_loop State.new events: :orddict.new, clients: :orddict.new
     end
 
-    def subscribe(pid) do
-        ref = :erlang.monitor :process, :erlang.whereis __MODULE__
-        __MODULE__ <- { Process.self, ref, {:subscribe, pid} }
-        receive do
-        match: {ref, :ok }
-            { :ok, ref }
-        match: { 'DOWN', ref, :process, _pid, reason }
-            { :error, reason }
-        after: 5000
-            { :error, :timeout }
-        end
-    end
-
+    # Create a new event with a unique name
     def add_event(name, description, timeout) do
         ref = :erlang.make_ref
         __MODULE__ <- { Process.self, ref, {:add, name, description, timeout} }
         receive do
-        match: { ref, msg }
+        match: { ^ref, msg }
             msg
         after: 5000
             { :error, :timeout }
         end
     end
 
+    # Subscribe to event notifications
+    def subscribe(pid) do
+        mon = :erlang.monitor :process, :erlang.whereis __MODULE__
+        __MODULE__ <- { Process.self, mon, {:subscribe, pid} }
+        receive do
+        match: { ^mon, :ok }
+            { :ok, mon }
+        match: { 'DOWN', ^mon, :process, ^pid, reason }
+            { :error, reason }
+        after: 5000
+            { :error, :timeout }
+        end
+    end
+
+    # Cancel the event with name 'name'
     def cancel(name) do
         ref = :erlang.make_ref
         __MODULE__ <- { Process.self, ref, {:cancel, name} }
         receive do
-        match: { ref, :ok }
+        match: { ^ref, :ok }
             :ok
         after: 5000
             { :error, :timeout }
         end
     end
 
-    def listen(delay) do
-        delay_msec = delay * 1000
+    # Shut down the server
+    def terminate do
+        __MODULE__ <- :shutdown
+    end
 
+    # Wait until all events have fired or the timeout has passed
+    def listen(delay) do
         receive do
         match: m = { :done, _name, _description }
             [m | listen(0)]
-        after: ^delay_msec
+        after: delay * 1000
             []
         end
     end
 
+
+    ## Private functions ##
+
+    # The main receive loop
+    defp main_loop(state) do
+        receive do
+        match: { pid, msg_ref, {:subscribe, client} }
+            # We'll keep a list of all subscribers and monitor them
+            # so that we don't send useless messages to crashed clients
+            mon = :erlang.monitor :process, client
+            new_state = state.update_clients fn(clients) -> :orddict.store mon, client, clients end
+            pid <- { msg_ref, :ok }
+            main_loop new_state
+
+        match: { pid, msg_ref, {:add, name, description, timeout} }
+            if timeout > 0 do
+                # Use the fully qualified name __MAIN__.Event to refer to the
+                # Event module and not our referred EventServer.Event record
+                event_pid = __MAIN__.Event.start_link name, timeout
+                new_state = state.update_events fn(events) ->
+                                :orddict.store name,
+                                Event.new(name: name, description: description, pid: event_pid, timeout: timeout),
+                                events
+                            end
+                pid <- { msg_ref, :ok }
+                main_loop new_state
+            else:
+                pid <- { msg_ref, {:error, :bad_timeout} }
+                main_loop state
+            end
+
+        match: { pid, msg_ref, {:cancel, name} }
+            # If the event with the specified name is not found,
+            # we simply do nothing
+            events = case :orddict.find(name, state.events) do
+                     match: { :ok, event }
+                         __MAIN__.Event.cancel event.pid
+                         :orddict.erase name, state.events
+                     match: :error
+                         state.events
+                     end
+            pid <- { msg_ref, :ok }
+            main_loop state.events events
+
+        match: { :done, name }
+            # The event has finished, notify all clients
+            case :orddict.find(name, state.events) do
+            match: { :ok, event }
+                send_to_clients { :done, event.name, event.description }, state.clients
+                main_loop state.update_events fn(events) -> :orddict.erase name, events end
+            match: :error
+                # This happens if we cancel an event and it fires at the same time
+                main_loop state
+            end
+
+        match: :shutdown
+            # Since we create each new event by calling `start_link`, all event
+            # processes will also be terminated.
+            exit :shutdown
+
+        match: { 'DOWN', ref, :process, _pid, _reason }
+            # A client has crashed
+            main_loop state.update_clients fn(clients) -> :orddict.erase ref, clients end
+
+        match: :code_change
+            # New code has arrived! Time to upgrade.
+            # The upgrade process is performed by using the qualified name
+            # __MODULE__.main_loop. Calling 'main_loop' instead would continue
+            # running the old code.
+            __MODULE__.main_loop state
+
+        match: else
+            # Someone sent us a message we don't understand
+            :io.format "Unknown message: ~p~n", [else]
+            main_loop state
+        end
+    end
+
+
+    # Send 'msg' to each subscribed client
     defp send_to_clients(msg, clients) do
         :orddict.map fn(_ref, pid) ->
             pid <- msg
